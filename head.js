2ebb0ddd5063e0b040b48a04541a9f1ff7331e4a
Jasmine <jasmine_singh1992@hotmail.com>
2020-02-04 05:27:07 +0000

    Added head.js which contains a function to print the head of an array

--- head.js
+++ head.js
@@ -1,3 +1,6 @@
+const head = function(array) {
+  return array[0];
+}
 const assertEqual = function(actual, expected) {
   if (actual === expected ) {
     console.log("Assertion Passed: " +actual +" === " +expected );
@@ -7,6 +10,6 @@ const assertEqual = function(actual, expected) {
   };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// assertEqual(head([5,6,7]), 5);
+
 
