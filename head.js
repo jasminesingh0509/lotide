dc6fd100e05d0d1b2fabf8e7271699ae2a23e8a4
Jasmine <jasminegit config --global user.email jasmine_singh1992@hotmail.comgit config --global user.name Your>
2020-02-10 21:34:45 +0000

    module.exports added

--- head.js
+++ head.js
@@ -1,13 +1,9 @@
+const assertEqual = require('./assertEqual');
 const head = function(array) {
   return array[0];
 }
-const assertEqual = function(actual, expected) {
-  if (actual === expected ) {
-    console.log("Assertion Passed: " +actual +" === " +expected );
-  } else  {
-    console.log( "Assertion Failed: "+actual +"!==" +expected ) ;
-  } 
-  };
+
+  
 
 // TEST CODE
 // assertEqual(head([5,6,7]), 5);
---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual'); 
+
+
+//Test code 
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+module.exports = assertEqual;
+
