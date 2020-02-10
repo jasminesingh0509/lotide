a90e79e4975e7a7ac9a888ba837a0cf363325741
Jasmine <jasminegit config --global user.email jasmine_singh1992@hotmail.comgit config --global user.name Your>
2020-02-10 23:17:22 +0000

    module exports

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,19 @@
+const assertEqual = require('../assertEqual');
+
+const assertArraysEqual = function(array1, array2) {
+  if (array1 === undefined || array2 === undefined || 
+      array1.length !== array2.length) 
+        {
+          return false;
+        }
+  for (let i = 0; i < array1.length; i++) {
+          if (array1[i] !== array2[i]) 
+        {
+            return false;
+      }
+    }
+  return true;
+};
+
+
+
