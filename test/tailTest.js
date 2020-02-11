5b864b36adda8507831156bb552e7a399208ac67
Jasmine <jasminegit config --global user.email jasmine_singh1992@hotmail.comgit config --global user.name Your>
2020-02-11 00:23:57 +0000

    chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -2,3 +2,18 @@ const assertEqual = require('../assertEqual');
 const tail = function(array) {
   return array.slice(1);
 };
+
+
+  
+
+
+
+const assert = require('chai').assert;
+
+
+describe("#tail", () => {
+  it("returns 3 for [1, 2, 3]", () => {
+    assert.strictEqual(tail([1, 2, 3]), 3);
+    
+  });
+});
